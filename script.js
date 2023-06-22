53fa66fc58f5e829e9d6563d52b9dc9ede4b6c58
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-06-22 08:54:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const userName = document.getElementById('name');
+const password = document.getElementById('password');
+const checkbox = document.getElememtById('checkbox');
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', () => {
+	if(checkbox.checked){
+		localStorage.setItem("username", userName.value);
+		localStorage.setItem("password", password.value);
+	}else{
+		localStorage.removeItem("username");
+		localStorage.removeItem("password");
+	}
+});
+
